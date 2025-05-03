99c961062a968571e5bb1bbdfba4f56ef9067ebd
Eugene Ostroukhov <eostroukhov@gmail.com>
2025-05-03 13:20:43 -0700

    Initial Commit

---
+++ LearningMetal/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LearningMetal
+//
+//  Created by Yevgen Ostroukhov on 5/3/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ LearningMetalTests/LearningMetalTests.swift
@@ -0,0 +1,17 @@
+//
+//  LearningMetalTests.swift
+//  LearningMetalTests
+//
+//  Created by Yevgen Ostroukhov on 5/3/25.
+//
+
+import Testing
+@testable import LearningMetal
+
+struct LearningMetalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
